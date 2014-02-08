b7fe7f3f80310ba3fc29114ec4e15f20d89f7be2
jayshao830413 <shaoshijie830413@foxmail.com>
2014-02-08 14:08:03 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RichEditorDemo::Application.config.secret_key_base = '14901d66cd33f2521cbca451cfa3d38a88df5d20f0a6b2440ea2ea92bd9cebddbc8d22e11d6c0e83be95351d8c281915bc1abde7972db12269a4544c4ed5f943'
